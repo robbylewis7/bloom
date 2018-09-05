68e12b26549b0cbd6b3b73b54b2f06ff562a2019
Robby Lewis <robbyklew@gmail.com>
2018-09-04 22:31:49 -0700

    adjusted travis files and server

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080);
 
-if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function() {
-    console.info(`App listening on ${this.address().port}`);
-  });
-}
+//if (require.main === module) {
+//  app.listen(process.env.PORT || 8080, function() {
+//    console.info(`App listening on ${this.address().port}`);
+//  });
+//}
 
 module.exports = app;
