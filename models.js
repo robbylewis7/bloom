0a47f1914b881a634a08b7c3e10fadde89d42395
Robby Lewis <robbyklew@gmail.com>
2018-09-05 22:30:16 -0700

    updated models, server, and config files

---
+++ models.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dailyTrakerSchema = new Schema({
+  
+  date: { type: Date, default: Date.now },
+  sleepStartHr: Number,
+  sleepStartMin: Number,
+  sleepEndHr: Number,
+  sleepEndMin: Number,
+  stress: Number,
+  gratidude: Number,
+  energy: Number,
+  community: Number,
+  waterIntake: Number,
+  cleanEating: Number,
+  exercise: Number,
+})
+
+function calculateSleepTotal(startHr, startMin, endHr, endMin) {
+  let calculatedSleepTotal;
+
+  if (startHr > endHr) {
+    // eg. 22 > 6
+    // eg. 22:30 > 6:15
+    calculatedSleepTotal =
+      24 - (startHr + startMin / 60) + (endHr + endMin / 60);
+  } else if (startHr < endHr) {
+    // eg. 0 < 7
+    calculatedSleepTotal = endHr + endMin / 60 - (startHr + startMin / 60);
+  } else {
+    // eg. slept @ 6, woke up @6:30
+    calculatedSleepTotal = endMin / 60 - startMin / 60;
+  }
+
+  calculatedSleepTotal = calculatedSleepTotal.toFixed(2);
+  return calculatedSleepTotal;
+}
+
+// VIRTUALS
+
+
+dailyTrakerSchema.virtual('sleepTotal').get(function() {
+  let sleepTotal = calculateSleepTotal(
+    this.sleepStartHr,
+    this.sleepStartMin,
+    this.sleepEndHr,
+    this.sleepEndMin
+  );
+  return sleepTotal;
+});
+
+// SERIALIZE
+
+dailyTrakerSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    dateAdjusted: this.dateAdjusted,
+    sleepTotal: this.sleepTotal,
+    stress: this.stress,
+    gratidude: this.gratidude,
+    energy: this.energy,
+    community: this.community,
+    waterIntake: this.waterIntake,
+    cleanEating: this.cleanEating,
+    exercise: this.exercise,
+  };
+};
+
+
+const Log = mongoose.model('Log', dailyTrakerSchema);
+
+module.exports = {Log};
--- server.js
+++ server.js
@@ -1,17 +1,63 @@
-"use strict";
+'use strict';
 
 const express = require('express');
-
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const { Log } = require('./models');
+
 app.use(express.static('public'));
+app.use(express.json());
 
-app.listen(process.env.PORT || 8080);
+app.get('/', (req, res) => {
+  res.status(200);
+  res.json('test');
+});
 
-//if (require.main === module) {
-//  app.listen(process.env.PORT || 8080, function() {
-//    console.info(`App listening on ${this.address().port}`);
-//  });
-//}
 
-module.exports = app;
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      databaseUrl,
+      err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app
+          .listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+          })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      }
+    );
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, server, runServer, closeServer };
